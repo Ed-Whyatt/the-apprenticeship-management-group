a52ef2da612a143bcfffe8933582b77b62b96684
Ed Whyatt <lizardxyz@googlemail.com>
2023-02-14 18:49:00 +0000

    Add hero image div animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,10 @@
 $(document).ready(function(){
     let pc = $('#pc');
     pc.animate({left: '200px'}, 1000);
+
+    let curveone = $('#curveone');
+    curveone.animate({left: '20px'}, 1500);
+
+    let curvetwo =$('#curvetwo');
+    curvetwo.animate({left: '20px'}, 1200)
 });
