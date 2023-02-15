a3ce86968855fa4cc67428e166530aaef537c4b9
Ed Whyatt <lizardxyz@googlemail.com>
2023-02-15 19:40:00 +0000

    Add header waves and first section animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
- console.log("hello world");
 $(document).ready(function(){
     let pc = $('#pc');
     pc.animate({left: '200px'}, 1000);
@@ -6,6 +5,18 @@ $(document).ready(function(){
     let curveone = $('#curveone');
     curveone.animate({left: '20px'}, 1500);
 
-    let curvetwo =$('#curvetwo');
-    curvetwo.animate({left: '20px'}, 1200)
+    let curvetwo = $('#curvetwo');
+    curvetwo.animate({left: '20px'}, 1200);
+
+    $('#sectionOneHeading').hide('slide', {direction: 'left'});
+    $('#sectionOneText').hide('slide', {direction: 'right'});
+    $('#headerLogoTwo').hide('slide', {direction: 'left'});
+    
+    
+    $(window).on('scroll',() => {
+        $('#sectionOneHeading').show('slide', {direction: 'left'}, 1000);
+        $('#sectionOneText').show('slide', {direction: 'right'}, 1000);
+        $('#headerLogoTwo').show('slide', {direction: 'left'}, 1000);
+    });
+    
 });
