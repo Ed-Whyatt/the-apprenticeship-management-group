fc773d7f3f20417153ff486a490f8a9ed0e5efa7
Ed Whyatt <lizardxyz@googlemail.com>
2023-02-23 20:52:42 +0000

    Add diagonal lines for divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     let pc = $('#pc');
     pc.animate({left: '200px'}, 1000);
+    
 
     let curveone = $('#curveone');
     curveone.animate({left: '20px'}, 1500);
@@ -16,7 +17,27 @@ $(document).ready(function(){
     $(window).on('scroll',() => {
         $('#sectionOneHeading').show('slide', {direction: 'left'}, 1500);
         $('#sectionOneText').show('slide', {direction: 'right'}, 1500);
-        $("#headerLogoTwo").fadeIn("slow");
+
+        // scroll point script
+        let headerLogoTwo = $('#headerLogoTwo').height();
+        if($(this).scrollTop()>=headerLogoTwo){
+            $("#headerLogoTwo").fadeIn("slow");
+            curveone.animate({left: '80px'}, 1500);
+            curvetwo.animate({left: '80px'}, 1200);
+            
+            // console.log("True")
+        } else {
+            $('#headerLogoTwo').hide('slide', {direction: 'left'});
+            console.log("else")
+        };
+
     });
     
+
+    // var scrollPosition = $("body, html").scrollTop()
+    // if (scrollPosition == 0){
+    //     console.log("True")
+    // };
+
+
 });
