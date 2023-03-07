a0bda6310120956cfb57730a8516cd5fbad45348
Ed Whyatt <lizardxyz@googlemail.com>
2023-03-07 14:59:25 +0000

    Add bottom image bounce and loge animation

---
+++ assets/js/oldScript.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    let pc = $('#pc');
+    pc.animate({left: '200px'}, 1000);
+    
+
+    let curveone = $('#curveone');
+    curveone.animate({left: '20px'}, 1500);
+
+    let curvetwo = $('#curvetwo');
+    curvetwo.animate({left: '20px'}, 1200);
+
+    $('#sectionOneHeading').hide('slide', {direction: 'left'});
+    $('#sectionOneText').hide('slide', {direction: 'right'});
+    $('#headerLogoTwo').hide('slide', {direction: 'left'});
+    $('#twoText').hide('slide', {direction: 'left'});
+    // console.log("Hey Dude Hope yor having an amazing day")
+    
+    
+    
+    $(window).on('scroll',() => {
+        $('#sectionOneHeading').show('slide', {direction: 'left'}, 1500);
+        $('#sectionOneText').show('slide', {direction: 'right'}, 1500);
+
+        // scroll point script
+        let headerLogoTwo = $('#headerLogoTwo').height();
+        if($(this).scrollTop()>=headerLogoTwo){
+            $("#headerLogoTwo").fadeIn("slow");
+            curveone.animate({left: '40px'}, 1500);
+            curvetwo.animate({left: '40px'}, 1200);
+
+        } else {
+            $('#headerLogoTwo').hide('slide', {direction: 'left'});
+        };
+        
+    });
+
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 1400) {
+            $('#twoText').show('slide', {direction: 'left'}, 1000);
+            $("#barchart").animate({
+                height: '300px',
+                width: '300px'
+              }, "slow");
+            
+        } 
+    });
+
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 2400) {
+            $('#imageOne').show('slide', {direction: 'left'}, 1000);
+            $('#imageTwo').show('slide', {direction: 'left'}, 1500);
+            $('#imageThree').show('slide', {direction: 'left'}, 2000);
+            $('#imageFour').show('slide', {direction: 'left'}, 2500);
+        } else {
+            $('#imageOne').hide('slide', {direction: 'left'});
+            $('#imageTwo').hide('slide', {direction: 'left'});
+            $('#imageThree').hide('slide', {direction: 'left'});
+            $('#imageFour').hide('slide', {direction: 'left'});
+        }
+    });
+
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,148 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let pc = $('#pc');
-    pc.animate({left: '200px'}, 1000);
-    
+    pc.animate({
+        left: '200px'
+    }, 1000);
 
     let curveone = $('#curveone');
-    curveone.animate({left: '20px'}, 1500);
+    curveone.animate({
+        left: '20px'
+    }, 1500);
 
     let curvetwo = $('#curvetwo');
-    curvetwo.animate({left: '20px'}, 1200);
+    curvetwo.animate({
+        left: '20px'
+    }, 1200);
 
-    $('#sectionOneHeading').hide('slide', {direction: 'left'});
-    $('#sectionOneText').hide('slide', {direction: 'right'});
-    $('#headerLogoTwo').hide('slide', {direction: 'left'});
-    $('#twoText').hide('slide', {direction: 'left'});
-    console.log("Hey Dude Hope yor having an amazing day")
-    
-    
-    
-    $(window).on('scroll',() => {
-        $('#sectionOneHeading').show('slide', {direction: 'left'}, 1500);
-        $('#sectionOneText').show('slide', {direction: 'right'}, 1500);
+    $('#navLogoTwo').hide('slide', {
+        direction: 'left'
+    });
+
+    $(window).on('scroll', () => {
 
         // scroll point script
-        let headerLogoTwo = $('#headerLogoTwo').height();
-        if($(this).scrollTop()>=headerLogoTwo){
-            $("#headerLogoTwo").fadeIn("slow");
-            curveone.animate({left: '40px'}, 1500);
-            curvetwo.animate({left: '40px'}, 1200);
+        let test = $('#navLogoOne').height();
+        if ($(this).scrollTop() >= test) {
+            curveone.animate({
+                left: '40px'
+            }, 1500);
+            curvetwo.animate({
+                left: '40px'
+            }, 1200);
 
-        } else {
-            $('#headerLogoTwo').hide('slide', {direction: 'left'});
         };
-        
+
     });
 
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 1400) {
-            $('#twoText').show('slide', {direction: 'left'}, 1000);
-            $("#barchart").animate({
-                height: '300px',
-                width: '300px'
-              }, "slow");
-            
-        } 
-    });
-
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 2400) {
-            $('#imageOne').show('slide', {direction: 'left'}, 1000);
-            $('#imageTwo').show('slide', {direction: 'left'}, 1500);
-            $('#imageThree').show('slide', {direction: 'left'}, 2000);
-            $('#imageFour').show('slide', {direction: 'left'}, 2500);
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 0) {
+            $('#navLogoOne').hide('slide', {
+                direction: 'left'
+            });
         } else {
-            $('#imageOne').hide('slide', {direction: 'left'});
-            $('#imageTwo').hide('slide', {direction: 'left'});
-            $('#imageThree').hide('slide', {direction: 'left'});
-            $('#imageFour').hide('slide', {direction: 'left'});
+            $('#navLogoOne').show('slide', {
+                direction: 'left'
+            });
         }
     });
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 200) {
+            $('#navLogoTwo').show('slide', {
+                direction: 'left'
+            });
+        } else {
+            $('#navLogoTwo').hide('slide', {
+                direction: 'left'
+            });
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 700) {
+            $('#imageOneContainer').show('slide', {
+                direction: 'left'
+            }, 1000);
+            $('#imageTwo').show('slide', {
+                direction: 'left'
+            }, 1500);
+            $('#imageThree').show('slide', {
+                direction: 'left'
+            }, 2000);
+            $('#imageFour').show('slide', {
+                direction: 'left'
+            }, 2500);
+        } else {
+            $('#imageOneContainer').hide('slide', {
+                direction: 'left'
+            });
+            $('#imageTwo').hide('slide', {
+                direction: 'left'
+            });
+            $('#imageThree').hide('slide', {
+                direction: 'left'
+            });
+            $('#imageFour').hide('slide', {
+                direction: 'left'
+            });
+        }
+    });
+
+    $("#imageOneContainer")
+        .mouseenter(function () {
+            $("#imageOneContainer").animate({
+                height: '160px',
+                width: '220px'
+            }, );
+        })
+        .mouseleave(function () {
+            $("#imageOneContainer").animate({
+                height: '150px',
+                width: '200px'
+            }, );
+        });
+
+    $("#imageTwoContainer")
+        .mouseenter(function () {
+            $("#imageTwoContainer").animate({
+                height: '160px',
+                width: '220px'
+            }, );
+        })
+        .mouseleave(function () {
+            $("#imageTwoContainer").animate({
+                height: '150px',
+                width: '200px'
+            }, );
+        });
+
+
+    $("#imageThreeContainer")
+        .mouseenter(function () {
+            $("#imageThreeContainer").animate({
+                height: '160px',
+                width: '220px'
+            }, );
+        })
+        .mouseleave(function () {
+            $("#imageThreeContainer").animate({
+                height: '150px',
+                width: '200px'
+            }, );
+        });
+
+    $("#imageFourContainer")
+        .mouseenter(function () {
+            $("#imageFourContainer").animate({
+                height: '160px',
+                width: '220px'
+            }, );
+        })
+        .mouseleave(function () {
+            $("#imageFourContainer").animate({
+                height: '150px',
+                width: '200px'
+            }, );
+        });
 
 });
